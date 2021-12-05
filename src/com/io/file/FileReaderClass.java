2f37c306cea1d0a2bf7d9512729f9a48e96ca985
adilkhan2 <adilkhan2@host.docker.internal>
2021-12-05 12:49:00 +0530

    File Reader Code

--- src/com/io/file/FileReaderClass.java
+++ src/com/io/file/FileReaderClass.java
@@ -1,8 +1,26 @@
 package com.io.file;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
 public class FileReaderClass {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+
+		FileReader fileReader = new FileReader("abc.txt");
+		int i = fileReader.read();
+
+		while (i != -1) {
+			System.out.print((char) i);
+			i = fileReader.read();
+		}
+
 	}
 }
+/*
+ * it is used to read character data from the file
+ * 
+ * 
+ */
