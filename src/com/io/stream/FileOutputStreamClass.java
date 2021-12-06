f100ebee5848a0c98edd887f1ccd65d04b61b4c7
Khan <adilkhan2@deloitte.com>
2021-12-07 00:11:33 +0530

    FOS msg

--- src/com/io/stream/FileOutputStreamClass.java
+++ src/com/io/stream/FileOutputStreamClass.java
@@ -18,6 +18,13 @@ public class FileOutputStreamClass {
 }
 /*
  * Java FileOutputStream is an output stream used for writing data to a file.
- * 
+ *
+ * If you have to write primitive values into a file, 
+ * use FileOutputStream class. You can write byte-oriented 
+ * as well as character-oriented data through FileOutputStream class.
+ * But, for character-oriented data, it is preferred to use FileWriter than
+ *  FileOutputStream.
+
+
  * 
  */
